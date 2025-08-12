9bec26efb8a00a250fa599854a3e18868938183d
blackpirateapps <57280276+blackpirateapps@users.noreply.github.com>
2025-08-12 18:38:33 +0530

    Update lastfm-proxy.js

--- netlify/functions/lastfm-proxy.js
+++ netlify/functions/lastfm-proxy.js
@@ -7,67 +7,92 @@ exports.handler = async (event) => {
     const API_URL = 'https://ws.audioscrobbler.com/2.0/';
 
     try {
+        const queryParams = event.queryStringParameters;
+
+        // Special action to get the login URL without exposing the API key
+        if (queryParams && queryParams.action === 'getLoginUrl') {
+            const apiKey = LASTFM_API_KEY;
+            const callback = queryParams.callback;
+            const authUrl = `https://www.last.fm/api/auth/?api_key=${apiKey}&cb=${callback}`;
+            
+            return {
+                statusCode: 200,
+                headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+                body: JSON.stringify({ authUrl }),
+            };
+        }
+
         let params;
         // The HTTP method (GET or POST) determines how we get the parameters
         if (event.httpMethod === 'GET') {
-            params = event.queryStringParameters;
-        } else {
+            params = queryParams;
+        } else { // POST
             params = JSON.parse(event.body);
         }
 
-        // Add the API key to every request
+        const isSignedRequest = params.method === 'auth.getSession' || params.method === 'track.love';
+        
+        // Always add API key for Last.fm requests
         params.api_key = LASTFM_API_KEY;
-        params.format = 'json';
 
         let finalUrl = API_URL;
         let options = {
             method: 'GET',
-            headers: { 'Content-Type': 'application/json' },
         };
 
-        // For methods that require a signature (authentication or writing data)
-        if (params.method === 'auth.getSession' || params.method === 'track.love') {
-            // Order parameters alphabetically
-            const orderedParams = Object.keys(params).sort().reduce((obj, key) => {
-                obj[key] = params[key];
+        if (isSignedRequest) {
+            // Signed requests must be POST and have a signature
+            options.method = 'POST';
+            
+            // Create a copy of params for signature, excluding 'format'
+            const paramsForSig = { ...params };
+            delete paramsForSig.format;
+
+            const orderedParams = Object.keys(paramsForSig).sort().reduce((obj, key) => {
+                obj[key] = paramsForSig[key];
                 return obj;
             }, {});
 
-            // Create the signature string
             let sig_string = '';
             for (const key in orderedParams) {
-                if (key !== 'format') { // format is not included in signature
-                   sig_string += key + orderedParams[key];
-                }
+                sig_string += key + orderedParams[key];
             }
             sig_string += LASTFM_API_SECRET;
             
-            // Add the MD5 hashed signature to the parameters
             params.api_sig = crypto.createHash('md5').update(sig_string, 'utf-8').digest('hex');
-
-            // Signed requests must be POST
-            options.method = 'POST';
+            
+            // All params, including api_sig, go into the form data body
             const formData = new URLSearchParams();
+            // Add format=json for the actual request to Last.fm
+            params.format = 'json';
             for (const key in params) {
                 formData.append(key, params[key]);
             }
             options.body = formData;
             options.headers = {'Content-Type': 'application/x-www-form-urlencoded'};
 
-        } else {
-            // For simple GET requests
+        } else { // Unsigned GET request
+            params.format = 'json';
             const queryString = Object.keys(params).map(key => `${key}=${encodeURIComponent(params[key])}`).join('&');
             finalUrl += `?${queryString}`;
         }
 
-        // Make the actual API call to Last.fm
         const response = await fetch(finalUrl, options);
         const data = await response.json();
 
-        // Return the data to the front-end
+        // Check for errors from the Last.fm API itself
+        if (data.error) {
+             console.error(`Last.fm API Error: ${data.message}`);
+             return {
+                statusCode: 400, // Bad Request or appropriate error code
+                headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
+                body: JSON.stringify(data),
+            };
+        }
+
         return {
             statusCode: 200,
-            headers: { "Access-Control-Allow-Origin": "*" }, // Allow requests from your site
+            headers: { "Content-Type": "application/json", "Access-Control-Allow-Origin": "*" },
             body: JSON.stringify(data),
         };
 
@@ -75,7 +100,7 @@ exports.handler = async (event) => {
         console.error('Error in Netlify function:', error);
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Internal Server Error' }),
+            body: JSON.stringify({ error: 'Internal Server Error', message: error.message }),
         };
     }
 };
